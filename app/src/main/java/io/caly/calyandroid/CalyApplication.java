10c2efb2af8a9249481a2d8c56e7487de0f27738
JSpiner <jspiner@naver.com>
2017-02-11 22:05:26 +0900

    orm setting

---
+++ app/src/main/java/io/caly/calyandroid/CalyApplication.java
@@ -0,0 +1,14 @@
+package io.caly.calyandroid;
+
+import android.app.Application;
+
+/**
+ * Copyright 2017 JSpiner. All rights reserved.
+ *
+ * @author jspiner (jspiner@naver.com)
+ * @project CalyAndroid
+ * @since 17. 2. 11
+ */
+
+public class CalyApplication extends Application {
+}
