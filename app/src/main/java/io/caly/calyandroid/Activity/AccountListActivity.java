6fedab10920343403d51f8ff4a172072a4a0570c
JSpiner <jspiner@naver.com>
2017-02-22 18:48:30 +0900

    add account add/list activity

---
+++ app/src/main/java/io/caly/calyandroid/Activity/AccountListActivity.java
@@ -0,0 +1,35 @@
+package io.caly.calyandroid.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import io.caly.calyandroid.R;
+
+/**
+ * Copyright 2017 JSpiner. All rights reserved.
+ *
+ * @author jspiner (jspiner@naver.com)
+ * @project CalyAndroid
+ * @since 17. 2. 22
+ */
+
+public class AccountListActivity extends AppCompatActivity {
+
+    //로그에 쓰일 tag
+    private static final String TAG = AccountListActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        init();
+    }
+
+    void init(){
+        ButterKnife.bind(this);
+    }
+}
