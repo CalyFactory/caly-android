5a4c9c43a3821db36c5b09ef1899540c813a4ea8
JSpiner <jspiner@naver.com>
2017-02-28 13:53:55 +0900

    create mapactivity, add naver map sdk

--- app/src/main/java/io/caly/calyandroid/Activity/MapActivity.java
+++ app/src/main/java/io/caly/calyandroid/Activity/MapActivity.java
@@ -5,21 +5,14 @@ import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-import java.util.ArrayList;
-import java.util.Objects;
+import com.nhn.android.maps.NMapView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import io.caly.calyandroid.Activity.Base.BaseAppCompatActivity;
-import io.caly.calyandroid.Adapter.EventListAdapter;
-import io.caly.calyandroid.Adapter.RecommandListAdapter;
-import io.caly.calyandroid.Model.EventModel;
 import io.caly.calyandroid.R;
 
 /**
@@ -27,32 +20,27 @@ import io.caly.calyandroid.R;
  *
  * @author jspiner (jspiner@naver.com)
  * @project CalyAndroid
- * @since 17. 2. 14
+ * @since 17. 2. 28
  */
 
-public class RecommandListActivity extends BaseAppCompatActivity {
+public class MapActivity extends BaseAppCompatActivity {
 
     @Bind(R.id.toolbar)
     Toolbar toolbar;
 
-    @Bind(R.id.recycler_recommandlist)
-    RecyclerView recyclerList;
-
-
-    RecommandListAdapter recyclerAdapter;
-    LinearLayoutManager layoutManager;
+    @Bind(R.id.mv_mapdetail)
+    NMapView mapView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recommandlist);
+        setContentView(R.layout.activity_map);
 
         init();
     }
 
     void init(){
         ButterKnife.bind(this);
-
 
         //set toolbar
         toolbar.setTitle("일정 목록");
@@ -65,26 +53,18 @@ public class RecommandListActivity extends BaseAppCompatActivity {
         });
 
         setSupportActionBar(toolbar);
+
         final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
         upArrow.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
         getSupportActionBar().setHomeAsUpIndicator(upArrow);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-        //set recyclerview
-        recyclerList.setHasFixedSize(true);
-
-        layoutManager = new LinearLayoutManager(getBaseContext());
-        recyclerList.setLayoutManager(layoutManager);
-
-
-        ArrayList<Object> dataList = new ArrayList<>();
-        for(int i=0;i<50;i++){
-            dataList.add(new Object());
-        }
-        recyclerAdapter = new RecommandListAdapter(dataList);
-        recyclerList.setAdapter(recyclerAdapter);
-
+        mapView.setClientId(getString(R.string.naver_client_id));
+        mapView.setClickable(true);
+        mapView.setEnabled(true);
+        mapView.setFocusable(true);
+        mapView.setFocusableInTouchMode(true);
+        mapView.requestFocus();
     }
 }
