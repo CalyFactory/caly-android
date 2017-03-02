e815737521fc018ef6889344e8e1c7012d413759
JSpiner <jspiner@naver.com>
2017-03-03 05:35:54 +0900

    add oauth

--- app/src/main/java/io/caly/calyandroid/Activity/TestActivity.java
+++ app/src/main/java/io/caly/calyandroid/Activity/TestActivity.java
@@ -2,10 +2,7 @@ package io.caly.calyandroid.Activity;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -18,16 +15,14 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import junit.framework.Test;
-
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.caly.calyandroid.Activity.Base.BaseAppCompatActivity;
 import io.caly.calyandroid.Model.Response.BasicResponse;
 import io.caly.calyandroid.R;
-import io.caly.calyandroid.Service.FirebaseMessagingService;
 import io.caly.calyandroid.Util.ApiClient;
 import io.caly.calyandroid.Util.Util;
+import io.caly.calyandroid.View.GoogleOAuthDialog;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -42,82 +37,20 @@ import retrofit2.Response;
 
 public class TestActivity extends BaseAppCompatActivity {
 
-
-    private static String CLIENT_ID = "881274260597-26b4e9bqah6nbp1katm5qlud3gfitlat.apps.googleusercontent.com";
-    //Use your own client id
-    private static String CLIENT_SECRET ="EEOeMFHQpLtaHDU4Rr8k-l3N";
-    //Use your own client secret
-    private static String REDIRECT_URI="https://ssoma.xyz:55566/googleAuthCallBack";
-    private static String GRANT_TYPE="authorization_code";
-    private static String TOKEN_URL ="https://accounts.google.com/o/oauth2/token";
-    private static String OAUTH_URL ="https://accounts.google.com/o/oauth2/auth";
-    private static String OAUTH_SCOPE="https://www.googleapis.com/auth/urlshortener";
-    //Change the Scope as you need
-    WebView web;
     Button auth;
-    SharedPreferences pref;
-    TextView Access;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        pref = getSharedPreferences("AppPref", MODE_PRIVATE);
-        Access =(TextView)findViewById(R.id.Access);
         auth = (Button)findViewById(R.id.auth);
         auth.setOnClickListener(new View.OnClickListener() {
             Dialog auth_dialog;
             @Override
             public void onClick(View arg0) {
                 // TODO Auto-generated method stub
-                auth_dialog = new Dialog(TestActivity.this);
-                auth_dialog.setContentView(R.layout.auth_dialog);
-                web = (WebView)auth_dialog.findViewById(R.id.webv);
-                web.getSettings().setJavaScriptEnabled(true);
-                web.loadUrl(OAUTH_URL+"?redirect_uri="+REDIRECT_URI+"&response_type=code&client_id="+CLIENT_ID+"&scope="+OAUTH_SCOPE+"&state=1234&sttttt=12345");
-                web.setWebViewClient(new WebViewClient() {
-
-                    boolean authComplete = false;
-                    Intent resultIntent = new Intent();
-
-                    @Override
-                    public void onPageStarted(WebView view, String url, Bitmap favicon){
-                        super.onPageStarted(view, url, favicon);
-
-                    }
-                    String authCode;
-                    @Override
-                    public void onPageFinished(WebView view, String url) {
-                        super.onPageFinished(view, url);
-
-                        Log.d(TAG, "onPageFinished : " + url);
-
-                        if (url.contains("?code=") && authComplete != true) {
-                            Uri uri = Uri.parse(url);
-                            authCode = uri.getQueryParameter("code");
-                            Log.i(TAG, "CODE : " + authCode);
-                            authComplete = true;
-                            resultIntent.putExtra("code", authCode);
-                            TestActivity.this.setResult(Activity.RESULT_OK, resultIntent);
-                            setResult(Activity.RESULT_CANCELED, resultIntent);
-
-                            SharedPreferences.Editor edit = pref.edit();
-                            edit.putString("Code", authCode);
-                            edit.commit();
-                            auth_dialog.dismiss();
-                            Toast.makeText(getApplicationContext(),"Authorization Code is: " +authCode, Toast.LENGTH_SHORT).show();
-                        }else if(url.contains("error=access_denied")){
-                            Log.i(TAG, "ACCESS_DENIED_HERE");
-                            resultIntent.putExtra("code", authCode);
-                            authComplete = true;
-                            setResult(Activity.RESULT_CANCELED, resultIntent);
-                            Toast.makeText(getApplicationContext(), "Error Occured", Toast.LENGTH_SHORT).show();
-
-                            auth_dialog.dismiss();
-                        }
-                    }
-                });
+                auth_dialog = new GoogleOAuthDialog(TestActivity.this);
                 auth_dialog.show();
-                auth_dialog.setTitle("Authorize Learn2Crack");
                 auth_dialog.setCancelable(true);
             }
         });
