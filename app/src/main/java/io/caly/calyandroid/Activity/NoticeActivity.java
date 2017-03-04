ce5694f181ebe87b25a40b3ed07391e0b6b30dfd
JSpiner <jspiner@naver.com>
2017-03-04 20:07:15 +0900

    add notice layout

--- app/src/main/java/io/caly/calyandroid/Activity/NoticeActivity.java
+++ app/src/main/java/io/caly/calyandroid/Activity/NoticeActivity.java
@@ -1,10 +1,24 @@
 package io.caly.calyandroid.Activity;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
+import java.util.ArrayList;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import io.caly.calyandroid.Activity.Base.BaseAppCompatActivity;
+import io.caly.calyandroid.Adapter.NoticeListAdapter;
+import io.caly.calyandroid.Adapter.SettingListAdapter;
+import io.caly.calyandroid.Model.DataModel.NoticeModel;
+import io.caly.calyandroid.Model.DataModel.SettingItemModel;
 import io.caly.calyandroid.R;
 
 /**
@@ -17,6 +31,15 @@ import io.caly.calyandroid.R;
 
 public class NoticeActivity extends BaseAppCompatActivity {
 
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Bind(R.id.recycler_notice)
+    RecyclerView recyclerList;
+
+    NoticeListAdapter recyclerAdapter;
+    RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +50,42 @@ public class NoticeActivity extends BaseAppCompatActivity {
 
     void init(){
         ButterKnife.bind(this);
+
+        //set toolbar
+        toolbar.setTitle("일정 목록");
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        setSupportActionBar(toolbar);
+
+        final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        upArrow.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //set recycler view
+        recyclerList.setHasFixedSize(true);
+
+        layoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+        recyclerList.setLayoutManager(layoutManager);
+
+        // test data
+        ArrayList<NoticeModel> dataList = new ArrayList<>();
+
+        dataList.add(new NoticeModel());
+        dataList.add(new NoticeModel());
+        dataList.add(new NoticeModel());
+        dataList.add(new NoticeModel());
+        dataList.add(new NoticeModel());
+
+        recyclerAdapter = new NoticeListAdapter(dataList);
+        recyclerList.setAdapter(recyclerAdapter);
     }
 
 }
--- app/src/main/java/io/caly/calyandroid/Adapter/NoticeListAdapter.java
+++ app/src/main/java/io/caly/calyandroid/Adapter/NoticeListAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.caly.calyandroid.CalyApplication;
+import io.caly.calyandroid.Model.DataModel.NoticeModel;
 import io.caly.calyandroid.Model.DataModel.TestModel;
 import io.caly.calyandroid.R;
 
@@ -25,13 +27,17 @@ public class NoticeListAdapter extends RecyclerView.Adapter<NoticeListAdapter.Vi
     //로그에 쓰일 tag
     private static final String TAG = CalyApplication.class.getSimpleName() + "/" + NoticeListAdapter.class.getSimpleName();
 
-    private ArrayList<TestModel> dataList;
+    private ArrayList<NoticeModel> dataList;
 
-    public NoticeListAdapter(ArrayList<TestModel> dataList){
+    public NoticeListAdapter(ArrayList<NoticeModel> dataList){
         this.dataList = dataList;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        @Nullable
+        @Bind(R.id.linear_notice_header)
+        public LinearLayout linearHeader;
 
         public ViewHolder(View view){
             super(view);
@@ -40,11 +46,6 @@ public class NoticeListAdapter extends RecyclerView.Adapter<NoticeListAdapter.Vi
 
         }
 
-        @Nullable
-        @OnClick(R.id.linear_notice_header)
-        void onHeaderClick(){
-
-        }
 
     }
 
@@ -66,19 +67,40 @@ public class NoticeListAdapter extends RecyclerView.Adapter<NoticeListAdapter.Vi
 
             viewHolder = new NoticeListAdapter.ViewHolder(view);
         }
-        else{
-            view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.item_notice_body, parent, false);
 
-            viewHolder = new NoticeListAdapter.ViewHolder(view);
-        }
 
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(NoticeListAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(NoticeListAdapter.ViewHolder holder, final int position) {
+        final NoticeModel noticeModel = dataList.get(position);
 
+        if(noticeModel.isHeader){
+            holder.linearHeader.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(noticeModel.isExpandabled){
+
+                        dataList.remove(position + 1);
+                        noticeModel.isExpandabled = false;
+                        notifyItemRemoved(position + 1);
+
+                    }
+                    else{
+                        dataList.add(position + 1, new NoticeModel(noticeModel.noticeDescription));
+                        noticeModel.isExpandabled = true;
+                        notifyItemInserted(position + 1);
+                    }
+                }
+            });
+        }
+
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return dataList.get(position).isHeader?1:2;
     }
 
     @Override
