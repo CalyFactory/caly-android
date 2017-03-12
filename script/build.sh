56c6c351188e5918867098aee6f0cc2654a843da
JSpiner <jspiner@naver.com>
2017-03-13 02:59:26 +0900

    test

--- script/build.sh
+++ script/build.sh
@@ -1,1 +1,20 @@
 ./gradlew build -stacktrace
+
+
+DEPLOY_BRANCH=$1
+DEPLOY_COMMIT=$2
+DEPLOY_COMMIT_MESSAGE=$3
+
+SLACK_KEY="xoxb-118976894853-""ypenVhWFgnlrfGmHXhrsImeq"
+SLACK_TEXT="[ `$DEPLOY_BRANCH` | `$DEPLOY_COMMIT` ] *$DEPLOY_COMMIT_MESSAGE* ${TRAVIS_COMMIT_MESSAGE:-none} "
+echo $SLACK_TEXT
+echo $TRAVIS_BRANCH
+
+echo $DEPLOY_BRANCH
+echo $DEPLOY_COMMIT
+echo $DEPLOY_COMMIT_MESSAGE
+curl \
+  -F "token=$SLACK_KEY" \
+  -F "channels=deploy-android" \
+  -F "text=$SLACK_TEXT" \
+  https://slack.com/api/chat.postMessage
