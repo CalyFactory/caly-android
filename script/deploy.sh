9144996e27aae9a98d33d8436e9d9c7731cf17a5
JSpiner <jspiner@naver.com>
2017-03-13 01:48:04 +0900

    change tag

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,12 +1,7 @@
-echo $1
-echo $2
-echo $3
-BRANCH_NAME=$1
-COMMIT_TAG=$2
-COMMIT_MSG=$3
 SLACK_KEY="xoxb-118976894853-""ypenVhWFgnlrfGmHXhrsImeq"
 SLACK_TEXT="[ `$1` |` $2` ] *$3* "
 echo $SLACK_TEXT
+echo $TRAVIS_BRANCH
 curl \
   -F "token=$SLACK_KEY" \
   -F "channels=deploy-android" \
