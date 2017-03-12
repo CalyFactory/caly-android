5befd2e06800d554b67948a23cf7727d0d5e0608
JSpiner <jspiner@naver.com>
2017-03-13 01:16:10 +0900

    fix travis code

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,1 @@
-echo $1
-echo $2
-echo $3
-BRANCH_NAME=$1
-COMMIT_TAG=$2
-COMMIT_MSG=$3
+./gradlew build -stacktrace
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,13 +1,12 @@
-ls
-ls app
-ls app/build
-ls app/build/outputs
-ls app/build/outputs/apk
+echo $1
+echo $2
+echo $3
 BRANCH_NAME=$1
 COMMIT_TAG=$2
 COMMIT_MSG=$3
 SLACK_KEY="xoxb-118976894853-""ypenVhWFgnlrfGmHXhrsImeq"
 SLACK_TEXT="[ `$BRANCH_NAME` |` $COMMIT_TAG` ] *$COMMIT_MSG* "
+echo $SLACK_TEXT
 curl \
   -F "token=$SLACK_KEY" \
   -F "channels=deploy-android" \
