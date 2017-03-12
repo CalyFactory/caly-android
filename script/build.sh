844a5499eaaee3b3e72401a1ed13a07ffd5a4d5d
JSpiner <jspiner@naver.com>
2017-03-12 21:52:23 +0900

    add slack code

--- script/build.sh
+++ script/build.sh
@@ -1,1 +1,8 @@
-curl "http://naver.com"
+echo $1
+SLACK_KEY="xoxb-118976894853-""ypenVhWFgnlrfGmHXhrsImeq"
+echo $SLACK_KEY
+curl \
+  -F "token=$SLACK_KEY" \
+  -F "channels=testbed" \
+  -F "file=@./app/build/outputs/apk/app-debug.apk" \
+  https://slack.com/api/files.upload
