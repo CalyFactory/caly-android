259781befaed74995e489c91b9476725a473c294
JSpiner <jspiner@naver.com>
2017-03-13 10:33:52 +0900

    chnage bulid code

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,3 @@
-./gradlew build -stacktrace
 
 
 DEPLOY_BRANCH=${1}
@@ -6,7 +5,7 @@ DEPLOY_COMMIT=${2}
 DEPLOY_COMMIT_MESSAGE=$3
 
 SLACK_KEY="xoxb-118976894853-""ypenVhWFgnlrfGmHXhrsImeq"
-SLACK_TEXT="[ `$DEPLOY_BRANCH` | `$DEPLOY_COMMIT` ] *$DEPLOY_COMMIT_MESSAGE* ${TRAVIS_COMMIT_MESSAGE:-none} "
+SLACK_TEXT="[ `{$DEPLOY_BRANCH}` | `{$DEPLOY_COMMIT}` ] *$DEPLOY_COMMIT_MESSAGE* ${TRAVIS_COMMIT_MESSAGE:-none} "
 echo $SLACK_TEXT
 echo $TRAVIS_BRANCH
 
@@ -18,3 +17,6 @@ curl \
   -F "channel=deploy-android" \
   -F "text=$SLACK_TEXT" \
   https://slack.com/api/chat.postMessage
+
+
+./gradlew build -stacktrace
