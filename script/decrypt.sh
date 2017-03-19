91ba0d455df6bd46756b28e4abecb5f909132792
JSpiner <jspiner@naver.com>
2017-03-19 23:47:31 +0900

    add signed apk key

--- script/decrypt.sh
+++ script/decrypt.sh
@@ -1,4 +1,3 @@
 openssl aes-256-cbc -K $encrypted_27342c7446d8_key -iv $encrypted_27342c7446d8_iv -in travis-encrypt.tgz.enc -out ./travis-encrypt.tgz -d
 tar xfvz travis-encrypt.tgz
-ls -l
-ls travis-encrypt 
+cp travis-encrypt/google-services.json app/google-services.json
