cb774d15e617cdce41f75797a1cc1b12e5ebeecd
JSpiner <jspiner@naver.com>
2017-03-23 12:48:34 +0900

    change deploy script to deploy debug+release apk

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,13 +4,16 @@ export DEPLOY_COMMIT="$TRAVIS_COMMIT"
 DEPLOY_COMMIT_MESSAGE=$3
 
 SLACK_KEY="xoxb-118976894853-""ypenVhWFgnlrfGmHXhrsImeq"
-SLACK_TEXT="[ \`$DEPLOY_BRANCH\` | \`$DEPLOY_COMMIT\` ] ${TRAVIS_COMMIT_MESSAGE:-none} "
-echo $SLACK_TEXT
-echo $TRAVIS_BRANCH
 
-echo $DEPLOY_BRANCH
-echo $DEPLOY_COMMIT
-echo $DEPLOY_COMMIT_MESSAGE
+SLACK_TEXT="[ *DEBUG* \`$DEPLOY_BRANCH\` | \`$DEPLOY_COMMIT\` ] ${TRAVIS_COMMIT_MESSAGE:-none} "
+curl \
+  -F "token=$SLACK_KEY" \
+  -F "channels=deploy-android" \
+  -F "initial_comment=$SLACK_TEXT" \
+  -F "file=@app/build/outputs/apk/app-debug.apk" \
+  https://slack.com/api/files.upload
+
+SLACK_TEXT="[ *RELEASE* \`$DEPLOY_BRANCH\` | \`$DEPLOY_COMMIT\` ] ${TRAVIS_COMMIT_MESSAGE:-none} "
 curl \
   -F "token=$SLACK_KEY" \
   -F "channels=deploy-android" \
