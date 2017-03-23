1deaa9167b8ddf1be9f493e7ba53bc7270ec5a7a
JSpiner <jspiner@naver.com>
2017-03-23 13:05:14 +0900

    change build version

---
+++ app/src/main/java/io/caly/calyandroid/Activity/ActivityLauncher.java
@@ -0,0 +1,12 @@
+package io.caly.calyandroid.Activity;
+
+/**
+ * Copyright 2017 JSpiner. All rights reserved.
+ *
+ * @author jspiner (jspiner@naver.com)
+ * @project CalyAndroid
+ * @since 17. 3. 23
+ */
+
+public class ActivityLauncher {
+}
