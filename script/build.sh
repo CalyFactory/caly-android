03bd840066a16349232eff1266c033753f14fc8f
JSpiner <jspiner@naver.com>
2017-04-05 15:33:58 +0900

    fix build deploy script

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,7 @@
+rm app/build/outputs/apk/*
+
+file_date=$(date "+%m.%d-%H:%M")
+
 #debug 
 ./gradlew assembleDebug \
 -Pandroid.injected.signing.store.file="travis-encrypt/calysigningkey.jks" \
@@ -5,6 +9,10 @@
 -Pandroid.injected.signing.key.alias=$KEY_ALIAS \
 -Pandroid.injected.signing.key.password=$KEY_PASSWORD
 
+file_name_debug="app/build/outputs/apk/app-debug_$file_date.apk"
+file_location_debug="app/build/outputs/apk/app-debug.apk"
+
+cp $file_location_debug $file_name_debug
 
 
 #release
@@ -13,3 +21,8 @@
 -Pandroid.injected.signing.store.password=$STORE_PASSWORD \
 -Pandroid.injected.signing.key.alias=$KEY_ALIAS \
 -Pandroid.injected.signing.key.password=$KEY_PASSWORD
+
+file_name_release="app/build/outputs/apk/app-release_$file_date.apk"
+file_location_release="app/build/outputs/apk/app-release.apk"
+
+cp $file_location_release $file_name_release
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,35 +1,41 @@
-export DEPLOY_BRANCH="$TRAVIS_BRANCH"
-export DEPLOY_COMMIT="$TRAVIS_COMMIT"
-DEPLOY_COMMIT_MESSAGE=$3
 
 
-file_date=$(date "+%m.%d-%H:%M")
+filename="$(find -name app-debug_*.apk)"
+
+if [ -z "$TRAVIS_BRANCH" ]
+  then
+    echo "deploy in local"
+    DEPLOY_BRANCH=$(git symbolic-ref --short HEAD)
+    DEPLOY_COMMIT=$(git log --pretty=format:'%h' -n 1)
+    DEPLOY_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+  else
+    echo "deploy in travis"
+    DEPLOY_BRANCH="$TRAVIS_BRANCH"
+    DEPLOY_COMMIT=$(git log --pretty=format:'%h' -n 1)
+    DEPLOY_COMMIT_MESSAGE=$3
+fi
+
+echo $DEPLOY_BRANCH
+echo $DEPLOY_COMMIT
+echo $DEPLOY_COMMIT_MESSAGE
 
 
-file_name_debug="app/build/outputs/apk/app-debug_$file_date.apk"
-file_location_debug="app/build/outputs/apk/app-debug.apk"
-
-echo $file_name_debug
-cp $file_location_debug $file_name_debug
-
-SLACK_TEXT="[ *DEBUG* \`$DEPLOY_BRANCH\` | \`$DEPLOY_COMMIT\` ] ${TRAVIS_COMMIT_MESSAGE:-none} "
+#: <<'END'
+SLACK_TEXT="[ *DEBUG* \`$DEPLOY_BRANCH\` | \`$DEPLOY_COMMIT\` ] ${DEPLOY_COMMIT_MESSAGE:-none} "
 curl \
   -F "token=$SLACK_KEY" \
   -F "channels=deploy-android" \
   -F "initial_comment=$SLACK_TEXT" \
-  -F "file=@$file_name_debug" \
+  -F "file=@$filename" \
   https://slack.com/api/files.upload
 
+filename="$(find -name app-release_*.apk)"
 
-file_name_release="app/build/outputs/apk/app-release_$file_date.apk"
-file_location_release="app/build/outputs/apk/app-release.apk"
-
-cp $file_location_release $file_name_release
-
-SLACK_TEXT="[ *RELEASE* \`$DEPLOY_BRANCH\` | \`$DEPLOY_COMMIT\` ] ${TRAVIS_COMMIT_MESSAGE:-none} "
+SLACK_TEXT="[ *RELEASE* \`$DEPLOY_BRANCH\` | \`$DEPLOY_COMMIT\` ] ${DEPLOY_COMMIT_MESSAGE:-none} "
 curl \
   -F "token=$SLACK_KEY" \
   -F "channels=deploy-android" \
   -F "initial_comment=$SLACK_TEXT" \
-  -F "file=@$file_name_release" \
+  -F "file=@$filename" \
   https://slack.com/api/files.upload
+#END
